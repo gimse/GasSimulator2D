289f69249cead8fc1728f5a935018c5e00ebe7d5
Håkon Gimse <23360355+gimse@users.noreply.github.com>
2020-08-16 12:21:02 +0200

    fungerer bra!

--- gas2d.py
+++ gas2d.py
@@ -17,17 +17,21 @@ x=np.linspace(0,L,n);
 y=np.linspace(0,L,n);
 (xx,yy)=np.meshgrid(x,y)
 
+pointsx=range(n//4,n//4+20);
+pointsy=[n//2] * 20
+
+
 
 vx=np.zeros((n,n));
-vx[n//4,n//2]=0.02;
+vx[pointsx,pointsy]=0.04;
 
 Bvx=np.logical_or(xx==0,xx==L)
-Bvx[n//4,n//2]=True;
+Bvx[pointsx,pointsy]=True;
 
 vy=np.zeros((n,n));
-vy[n//4,n//2]=0.005;
+vy[pointsx,pointsy]=0.01;
 Bvy=np.logical_or(yy==0,yy==L)
-Bvy[n//4,n//2]=True;
+Bvy[pointsx,pointsy]=True;
 
 
 de=np.ones((n,n))*1.2754
@@ -80,22 +84,25 @@ u=u0;
 sol=RK45(uDerivative,0,u,100,vectorized=False)
 
 
-for i in range(300):
+for i in range(500):
     sol.step()
-
+    print('t:',sol.t)
     u=sol.y
-    print()
     #u=u+0.0005*uDerivative(0,u)
     
     up=np.reshape(u,(3,n*n))
     up=np.reshape(up,(3,n,n))
     
-    
-    plot_indexs=np.rint(np.linspace(0,n-1,9)).astype(int)
+    n_new=18;
+    plot_indexs=np.rint(np.linspace(0,n-1,n_new)).astype(int)
+    (plot_indexsxx,plot_indexsyy)=np.meshgrid(plot_indexs,plot_indexs)
+    plot_indexsyy=np.reshape(plot_indexsyy,n_new*n_new)
+    plot_indexsxx=np.reshape(plot_indexsxx,n_new*n_new)
 
     
+    
     fig, ax = plt.subplots()
-    q = ax.quiver(xx[plot_indexs,plot_indexs],yy[plot_indexs,plot_indexs],up[0,plot_indexs,plot_indexs],up[1,plot_indexs,plot_indexs],scale=0.1)
+    q = ax.quiver(xx[plot_indexsxx,plot_indexsyy],yy[plot_indexsxx,plot_indexsyy],up[0,plot_indexsxx,plot_indexsyy],up[1,plot_indexsxx,plot_indexsyy],scale=1)
     plt.show()
 
 
